9fae939bb984b568a7b26c21404e1851a53c0eed
fatiherdogan <fatiherdogan_@windowslive.com>
2021-08-30 12:51:17 +0300

    add status bar

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import MusicListScreen from "./src/screens/MusicListScreen";
+import { Colors } from "./src/style/Colors";
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar
+        animated={true}
+        backgroundColor={Colors.header_gray}
+        barStyle={"dark-content"}
+      />
       <MusicListScreen />
     </SafeAreaView>
   );
